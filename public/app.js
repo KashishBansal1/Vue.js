3f94d6e8d9a6da7303cfff3b49600d71d310dbea
Kashish Bansal <aashma.2008@gmail.com>
2022-07-01 00:24:21 +0530

    vue cli

---
+++ public/app.js
@@ -0,0 +1,15 @@
+
+import Vue from 'vue'
+
+Vue.component('click-counter', {
+    template: '<button @click="count++">{{count}}</button>',
+    data() {
+        return {
+            count: 0
+        }
+    }
+})
+
+new Vue({
+    el: '#app'
+})
